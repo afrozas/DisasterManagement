b5a29b3b417ae0e17ee6deed12682a35bc363d49
ankit <ankit0905anand@gmail.com>
2018-10-25 17:01:42 +0530

    Fix donation/request view

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,11 +8,16 @@ def index(request):
     context = {'keywords': KEYWORDS}
     if request.method == "POST":
         form_data = request.POST
-        req = Request()
-        req.person_name = form_data.get('name', '')
-        req.keywords = ','.join(form_data.getlist('items', []))
-        if 'request' not in request.POST:
-            req.donation = True
+        for keyword in form_data.getlist('items', []):
+            req = Request()
+            req.person_name = form_data.get('name', '')
+            req.keywords = keyword
+            if 'request' not in request.POST:
+                req.donation = True
+            req.phone_num = form_data.get('phone', '')
+            req.description = form_data.get('description', '')
+            # print("Request: " + req.person_name + " " + req.keywords + \
+            #       " " + req.description + " " + req.phone_num)
         req.save()
         context['success'] = True
     return render(request, 'dashboard/index.html', context)
