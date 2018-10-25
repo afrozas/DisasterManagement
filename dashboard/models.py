9eea1456a260b3b7ac68da99b4720748313dae0c
mach64 <krutpatel99@gmail.com>
2018-10-25 15:34:22 +0530

    dashboard: Save user request to db

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Request(models.Model):
-    keywords = models.CharField(max_length=200) # JSON string of list of keywords
-    latitude = models.DecimalField(max_digits=10, decimal_places=5)
-    longitude = models.DecimalField(max_digits=10, decimal_places=5)
-    description = models.CharField(max_length=500)
+    person_name = models.CharField(max_length=50, blank=True)
+    keywords = models.CharField(max_length=200)  # CSV of keywords
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+    description = models.CharField(max_length=500, blank=True)
+    phone_num = models.CharField(max_length=15, blank=True)
+    donation = models.BooleanField(default=False)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,18 +1,18 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Request
 
 
 def index(request):
-    KEYWORDS = ["Food", "Water", "Medicines", "Clothing", "Appliances", "Others"]
+    KEYWORDS = ["Food", "Water", "Medicines",
+                "Clothing", "Appliances", "Others"]
     context = {'keywords': KEYWORDS}
     if request.method == "POST":
-        if 'request' in request.POST:
-            print("Items Requested")
-        else:
-            print("Items Donated")
-        print(request.POST.get('name'))
-        print(request.POST.getlist('items'))
-        print(request)
-        return render(request, 'dashboard/index.html', context)
-    else:
-        return render(request, 'dashboard/index.html', context)
+        form_data = request.POST
+        req = Request()
+        req.person_name = form_data.get('name', '')
+        req.keywords = ','.join(form_data.getlist('items', []))
+        if 'request' not in request.POST:
+            req.donation = True
+        req.save()
+        context['success'] = True
+    return render(request, 'dashboard/index.html', context)
