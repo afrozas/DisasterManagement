2a386f6c96508d6ef998d639a0aaac9daf1ff3c1
mach64 <krutpatel99@gmail.com>
2018-11-15 15:53:04 +0530

    Synchronize cluster points and centers

--- clustering/views.py
+++ clustering/views.py
@@ -1,55 +1,47 @@
 import base64
 import json
+from collections import defaultdict
 from django.shortcuts import render
 
 
-def read_cluster_center_data():
+def read_cluster_data():
     with open('clustering/data.json') as f:
         clusters = json.load(f)
-    imgs = []
-    data = []
-    lat = []
-    lon = []
-    for cluster in clusters:
+    cluster_centers = defaultdict(list)
+    all_points_data = {}
+    for index, cluster in enumerate(clusters):
         try:
             with open(cluster['figure_path'], 'rb') as f:
-                imgs.append(base64.b64encode(f.read()).decode('utf-8'))
+                cluster_centers['imgs'].append(
+                    base64.b64encode(f.read()).decode('utf-8'))
         except FileNotFoundError:
-            imgs.append('')
-        data.append(cluster['data'])
-        lat.append(cluster['latitude'])
-        lon.append(cluster['longitude'])
-    return imgs, data, lat, lon
-
-
-def read_all_points_data():
-    with open('clustering/all_points.json') as f:
-        all_points = json.load(f)
-    data = {}
-    for num, points in sorted(all_points.items(), key=lambda x: int(x[0])):
-        lat = []
-        lon = []
-        keywords = []
-        for point in points:
+            print("SDSDFSDFSDF")
+            cluster_centers['imgs'].append(None)
+        cluster_centers['lat'].append(cluster['latitude'])
+        cluster_centers['lon'].append(cluster['longitude'])
+        lat, lon, text = [], [], []
+        for point in cluster['points']:
             lat.append(point['lat'])
             lon.append(point['log'])
-            keywords.append(point['keywords'])
-        data[num] = {'lat': json.dumps(
-            lat), 'lon': json.dumps(lon), 'text': json.dumps(keywords)}
-    return data
+            text.append(point['keywords'])
+        all_points_data[index] = {'lat': json.dumps(
+            lat), 'lon': json.dumps(lon), 'text': json.dumps(text)}
+    return cluster_centers, all_points_data
 
 
 def show_map(request):
-    imgs, _, lat, lon = read_cluster_center_data()
+    centers, all_points = read_cluster_data()
+    lat, lon = centers['lat'], centers['lon']
+
     data = {
         'centers': {
             'lat': lat,
             'lon': lon,
-            'imgs': json.dumps(imgs),
+            'imgs': json.dumps(centers['imgs']),
             'text': json.dumps([f'Cluster {i}' for i in range(1, len(lat) + 1)]),
             'lat_center': sum(lat) / len(lat),
             'lon_center': sum(lon) / len(lon)
         },
-        'all': read_all_points_data()
+        'all': all_points
     }
     return render(request, 'clustering/show_map.html', data)
